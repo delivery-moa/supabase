13eababd9658c573becd35e1a1216b6a032676ed
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-29 12:47:45 +0900

    Update realtime

---
+++ enable-realtime.sql
@@ -0,0 +1,5 @@
+ALTER PUBLICATION supabase_realtime ADD TABLE "room";
+ALTER PUBLICATION supabase_realtime ADD TABLE "room_join";
+ALTER PUBLICATION supabase_realtime ADD TABLE "chat";
+ALTER PUBLICATION supabase_realtime ADD TABLE "order";
+ALTER PUBLICATION supabase_realtime ADD TABLE "user";
